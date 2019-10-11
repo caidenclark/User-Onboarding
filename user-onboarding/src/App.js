d6fdbf8de0c9ee5c2c35ca65e4a2c848f30a50fd
Lambda_School_Loaner_181 <lambda_school_loaner_181@Lambda-School-Loaner-181s-MacBook-Pro.local>
2019-10-10 21:40:05 -0500

    MVP

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import FormikLogIn from './components/LogInForm';
+
+function App() {
+  return (
+    <FormikLogIn />
+  )
+}
+
+export default App;
